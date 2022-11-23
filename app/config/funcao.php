abb5279e6ebad35d796df6e83a920babe352ba4d
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2022-11-23 16:51:02 -0300

    Função para realizar a movimentação

--- app/config/funcao.php
+++ app/config/funcao.php
@@ -7,3 +7,37 @@ function LogSistema($con, $acao, $descricao, $usuario)
     $query = "insert into logalteracao(acao,descricao,idusuario,data,hora) values ('{$acao}','{$descricao}',{$usuario},'{$data}', '{$hora}')";
     mysqli_query($con, $query);
 }
+
+
+function movimentarProduto($con, $tipo, $precusto, $prevenda, $quantidade, $produto, $estoque)
+{
+    $precusto = isset($precusto) ? $precusto : 'null';
+    $prevenda = isset($prevenda) ? $prevenda : 'null';
+    $estoque = isset($estoque) ? $estoque : 'null';
+
+    $data = date('Y-m-d');
+    $hora = date('H:i:s');
+    $queryMov = "insert into movimentacao(
+                                            idusuario,
+                                            idproduto,
+                                            idestoque,
+                                            tipo,
+                                            precusto,
+                                            prevenda,
+                                            quantidade,
+                                            criado_data,
+                                            criado_hora
+                                        ) values (
+                                            {$_SESSION['idusuario']},
+                                            {$produto},
+                                            {$estoque},
+                                            '{$tipo}',
+                                            {$precusto},
+                                            {$prevenda},
+                                            {$quantidade},
+                                            '{$data}',
+                                            '{$hora}'
+                                        )";
+
+    mysqli_query($con, $queryMov);
+}
